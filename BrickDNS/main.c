b6a2f2ebf07901e5c7d98d2a52773b5bd0057f41
Alec Zadikian <alecz@berkeley.edu>
2014-10-04 06:23:55 -0700

    commit

--- BrickDNS/main.c
+++ BrickDNS/main.c
@@ -32,8 +32,11 @@
 #define MAX_PENDING 9000
 
 int main(int argc, const char * argv[]){
-    if (argc==2) logLevel = (atoi(argv[1])>LOG_FULL)?LOG_FULL:atoi(argv[1]);
-    else logLevel = LOG_INFORMATIONAL;
+    if (argc!=5){
+        printf("Invalid usage. Start with: brickdns [logLevel] [threadLimit] [timeout] [keepAlive]\n");
+        return 0;
+    }
+    logLevel = (atoi(argv[1])>LOG_FULL)?LOG_FULL:atoi(argv[1]);
     if (logLevel > LOG_NONE){
         printf("Starting BrickDNS server on port %d...\n", PORT);
     }
@@ -57,11 +60,12 @@ int main(int argc, const char * argv[]){
     
     struct Server server; // TODO change these to what we want
     server.sockfd = servSock;
-    server.threadLimit = 16;
-    server.timeout = 100;
-    server.keepAlive = true;
+    server.threadLimit = atoi(argv[2]); //16;
+    server.timeout = atof(argv[3]); //100;
+    server.keepAlive = atoi(argv[4]); // true;
     server.isListening = true;
     server.recvSize = 1024;
+    if (logLevel>=LOG_ABNORMAL) printf("logLevel: %d\nthreadLimit: %d\ntimeout: %f\nkeepAlive: %d\n",logLevel,server.threadLimit, server.timeout, server.keepAlive);
     
     startServer(&server);
     
